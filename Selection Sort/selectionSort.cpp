6a7929ad13170988f121c2c0c5b92f1dbbed7449
Guilherme Vieira <88863957+guilhermevieirasilvagoncalves@users.noreply.github.com>
2023-03-21 19:40:57 -0300

    Create selectionSort.cpp

---
+++ Selection Sort/selectionSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void selectionSort(int arr[], int n){
+
+    int i, j, min;
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i+1; j < n; j++) if (arr[j] < arr[min]) min = j;
+        int aux = arr[min];
+        arr[min] = arr[i];
+        arr[i] = aux;
+
+    }
+
+}
+
+int main() {
+    int arr[] = {10, 7, 3, 8, 4, 2, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selectionSort(arr, n);
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+
+}
